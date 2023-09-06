fd8c36f5097cccff4d4909bc71835394d64b362c
Phil <phil@upvalue.io>
2023-09-05 22:51:03 -0700

    remove database

--- app/queries/db.sql
+++
@@ -1,8 +0,0 @@
-/* @name listWorkspacesQuery */
-SELECT * FROM workspaces;
-
-/* @name insertWorkspaceMutation */
-INSERT INTO workspaces (slug, name) VALUES (:slug, :name);
-
-/* @name getWorkspaceQuery */
-SELECT * FROM workspaces WHERE slug = :slug;
